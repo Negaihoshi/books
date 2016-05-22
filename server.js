282b15867000afde0aec2af70a9c4f64d824173c
Negaihoshi <e222et@gmail.com>
2016-05-23 00:34:18 +0800

    add website parser

--- server.js
+++ server.js
@@ -2,7 +2,32 @@ let express = require('express');
 let app = express();
 let bodyParser = require('body-parser')
 let request = require('request');
-let parse5 = require('parse5');
+let cheerio = require('cheerio');
+let url = require('url');
+let log4js = require('log4js');
+log4js.replaceConsole();
+
+let requestAPI = (url) => {
+  return new Promise(function (resolve, reject) {
+    request(url, function (error, response, body) {
+      if (error) {
+        return reject(error);
+      }
+
+      return resolve(body);
+    });
+  });
+}
+
+var isEbook = function(string){
+  let chineseList = ["電子書", "二手書"];
+  for (var i = 0; i < chineseList.length; i++) {
+    if (string.indexOf(chineseList[i]) > -1) {
+      return chineseList[i];
+    }
+  }
+  return '實體書';
+};
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -12,19 +37,50 @@ app.get('/', (req, res) => {
 });
 
 app.post('/url', function (req, res, next) {
-  console.log(req.body);
+  let urlParse = url.parse(req.body.path);
+  console.log(urlParse.hostname);
+  switch (urlParse.hostname) {
+    case 'www.taaze.tw':
+    requestAPI(req.body.path).then((data) => {
+      let $ = cheerio.load(data);
+      let bookName =  $('body > div:nth-child(37) > h1:nth-child(1)').text();
+      let originPrice = $('#prodInfo3 > li:nth-child(2) > span:nth-child(3)').text();
+      let discount = $('#prodInfo3 > li:nth-child(3) > span:nth-child(1) > span:nth-child(1)').text();
+      let disCountPrice = $('#prodInfo3 > li:nth-child(3) > span:nth-child(2) > span:nth-child(1)').text();
+      // return originPrice + '_' + discount + '_'  + disCountPrice;
+      res.send('讀冊' + ' ' + bookName + '_' + originPrice + '_' + discount + '_'  + disCountPrice + '_' + isEbook(bookName));
+    });
+    break;
+    case 'www.books.com.tw':
+    requestAPI(req.body.path).then((data) => {
+      let $ = cheerio.load(data);
+      let bookName =  $('.type02_p002 > h1:nth-child(1)').text();
+      let originPrice = $('.price > li:nth-child(1) > em:nth-child(1)').text();
+      let discount = $('.price > li:nth-child(2) > strong:nth-child(1) > b:nth-child(1)').text();
+      let disCountPrice = $('.price01 > b:nth-child(2)').text();
+      // return originPrice + '_' + discount + '_'  + disCountPrice;
+      res.send('博客來' + ' ' + bookName + '_' + originPrice + '_' + discount + '_'  + disCountPrice + '_' + isEbook(bookName));
+    });
+    break;
+    case 'store.readmoo.com':
+    requestAPI(req.body.path).then((data) => {
+      let $ = cheerio.load(data);
+      let bookName =  $('.book-meta-box > h1:nth-child(1)').text();
+      let originPrice = $('div.price:nth-child(1) > del:nth-child(1) > strong:nth-child(1)').text();
+      let disCountPrice = $('div.price:nth-child(3) > strong:nth-child(1)').text();
+      let discount = (disCountPrice / originPrice) * 100;
+      // return originPrice + '_' + discount + '_'  + disCountPrice;
+      res.send('Readmoo' + ' ' + bookName + '_' + originPrice + '_' + discount + '_'  + disCountPrice + '_' + '電子書');
+    });
+      break;
+    default:
+    res.send('not support this website');
+  }
+
   // res.json(req.body);
 
-  request(req.body.path, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      // console.log(body) // Show the HTML for the Google homepage.
 
-      let document     = parse5.parse(body);
-      let documentHtml = parse5.serialize(document);
-      // console.log(documentHtml);
-      res.send(documentHtml);
-    }
-  })
+
 
 });
 
